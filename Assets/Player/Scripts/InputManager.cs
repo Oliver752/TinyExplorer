7d41f58072cd32b3888c641336245bbf6156f201
turcsekoliver <turcsek.oliver@gmail.com>
2025-11-16 01:04:28 +0100

    Player Finished (pickup & animation TBA); furniture hitboxes edited

--- Assets/Player/Scripts/InputManager.cs
+++ Assets/Player/Scripts/InputManager.cs
@@ -13,14 +13,13 @@ namespace UnityTutorial.Manager
         public Vector2 Look {get; private set;}
         public bool Run {get; private set;}
         public bool Jump {get; private set;}
-        //public bool Crouch {get; private set;}
-
+        public bool Crouch {get; private set;}
         private InputActionMap _currentMap;
         private InputAction _moveAction;
         private InputAction _lookAction;
         private InputAction _runAction;
         private InputAction _jumpAction;
-        //private InputAction _crouchAction;
+        private InputAction _crouchAction;
 
         private void Awake() {
             HideCursor();
@@ -29,19 +28,19 @@ namespace UnityTutorial.Manager
             _lookAction = _currentMap.FindAction("Look");
             _runAction  = _currentMap.FindAction("Run");
             _jumpAction = _currentMap.FindAction("Jump");
-            //_crouchAction = _currentMap.FindAction("Crouch");
+            _crouchAction = _currentMap.FindAction("Crouch");
 
             _moveAction.performed += onMove;
             _lookAction.performed += onLook;
             _runAction.performed += onRun;
             _jumpAction.performed += onJump;
-            //_crouchAction.started += onCrouch;
+            _crouchAction.started += onCrouch;
 
             _moveAction.canceled += onMove;
             _lookAction.canceled += onLook;
             _runAction.canceled += onRun;
             _jumpAction.canceled += onJump;
-            //_crouchAction.canceled += onCrouch;
+            _crouchAction.canceled += onCrouch;
         }
 
         private void HideCursor()
@@ -62,17 +61,14 @@ namespace UnityTutorial.Manager
         {
             Run = context.ReadValueAsButton();
         }
-        
         private void onJump(InputAction.CallbackContext context)
         {
             Jump = context.ReadValueAsButton();
         }
-        /*
         private void onCrouch(InputAction.CallbackContext context)
         {
             Crouch = context.ReadValueAsButton();
         }
-        */
         private void OnEnable() {
             _currentMap.Enable();
         }
@@ -80,6 +76,5 @@ namespace UnityTutorial.Manager
         private void OnDisable() {
             _currentMap.Disable();
         }
-        
     }
 }
