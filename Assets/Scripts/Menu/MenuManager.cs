48462987333739f850451bd41ff3a079d014d3c2
SimonaPoliakova <poliakova27712@gmail.com>
2025-11-22 15:01:42 +0100

    menu sounds + settings

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -1,11 +1,116 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    [Header("Panels")]
+    [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject settingsPanel;
+
+    [Header("Menu Sounds")]
+    [SerializeField] private AudioSource uiAudioSource;
+    [SerializeField] private AudioClip clickSound;
+
+    [Header("Background Music")]
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioClip backgroundMusic;
+
+    [Header("Volume Sliders")]
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    void Start()
+    {
+        mainMenuPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+
+        float savedMusicVolume = PlayerPrefs.GetFloat("MusicVolume", 0.6f);
+        float savedSFXVolume = PlayerPrefs.GetFloat("SFXVolume", 0.8f);
+
+        if (musicSlider != null)
+        {
+            musicSlider.value = savedMusicVolume;
+            musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        }
+
+        if (sfxSlider != null)
+        {
+            sfxSlider.value = savedSFXVolume;
+            sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+        }
+
+        if (musicSource != null)
+            musicSource.volume = savedMusicVolume;
+        if (uiAudioSource != null)
+            uiAudioSource.volume = savedSFXVolume;
+
+        PlayMenuMusic();
+    }
+
+    // ------------------- MENU ACTIONS -------------------
+
     public void StartGame()
     {
+        PlayClickSound();
         SceneManager.LoadScene("Scene");
     }
-    
+
+    public void QuitGame()
+    {
+        PlayClickSound();
+        Application.Quit();
+    }
+
+    public void OpenSettings()
+    {
+        PlayClickSound();
+        mainMenuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        PlayClickSound();
+        settingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    // ------------------- SOUND -------------------
+
+
+    public void PlayClickSound()
+    {
+        if (uiAudioSource != null && clickSound != null)
+            uiAudioSource.PlayOneShot(clickSound, sfxSlider != null ? sfxSlider.value : 1f);
+    }
+
+    private void PlayMenuMusic()
+    {
+        if (musicSource == null || backgroundMusic == null) return;
+
+        musicSource.clip = backgroundMusic;
+        musicSource.loop = true;
+        musicSource.Play();
+    }
+
+    // ------------------- VOLUME -------------------
+
+    public void SetMusicVolume(float volume)
+    {
+        if (musicSource != null)
+            musicSource.volume = volume;
+
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+        PlayerPrefs.Save();
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        if (uiAudioSource != null)
+            uiAudioSource.volume = volume;
+
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+        PlayerPrefs.Save();
+    }
 }
